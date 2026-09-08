4d106620f027504b98cbe99b477406fff7e8f56b
Umme Habeeba <habeeba13134@gmail.com>
2026-09-08 15:48:58 +0530

    Add hollow diamond pattern function
    Implement function to print a hollow diamond pattern based on input size.

--- python practice/practice/alphabet patterns/hallowdiamond.py
+++ python practice/practice/alphabet patterns/hallowdiamond.py
@@ -1,1 +1,22 @@
-
+def hallow_dimond(n):
+    width=2*n-1
+    for i in range(n):
+        for j in range(width):
+            if i==0 or i==n-1:
+                print("*",end="")
+            elif j==i or j==width-1-i:
+                print("*",end="")
+            else:
+                print("",end="")
+        print()
+    for i in range(n-2,-1,-1):
+        for j in range(width):
+            if i==0:
+                print("*",end="")
+            elif j==width-1-i:
+                print("*",end="")
+            else:
+                print("",end="")
+        print()
+n=4      
+hallow_dimond(n)
