79289205efca96323c101e91f3b550485391c2b2
Umme Habeeba <habeeba13134@gmail.com>
2026-09-08 11:34:59 +0530

    Add function to print right-aligned alphabet triangle

--- python practice/practice/alphabet patterns/right allign alphabet triangle.py
+++ python practice/practice/alphabet patterns/right allign alphabet triangle.py
@@ -1,1 +1,7 @@
-
+def right_aligned_alphabet_triangle(n):
+    for i in range(1,n+1):
+        print(""*(n-i),end="")
+        for j in range(i):
+            print(chr(65+j),end="")
+        print()    
+right_aligned_alphabet_triangle(6)
