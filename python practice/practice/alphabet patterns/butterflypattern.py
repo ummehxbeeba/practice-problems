28261d8502239e4c810a6ec2554869545a7e8c7b
Umme Habeeba <habeeba13134@gmail.com>
2026-09-08 12:16:13 +0530

    Add butterfly pattern function to print pattern

--- python practice/practice/alphabet patterns/butterflypattern.py
+++ python practice/practice/alphabet patterns/butterflypattern.py
@@ -1,1 +1,11 @@
-
+def butterfly_pattern(n):
+    for i in range(1,n+1):
+        print("*"*i,end="")
+        print(""*(2*(n-i)),end="")
+        print("*"*i)
+    for i in range(n-1,0,-1):
+         print("*"*i,end="")
+         print(""*(2*(n-i)),end="")
+         print("*"*i)
+n=4
+butterfly_pattern(n)
